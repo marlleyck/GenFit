37fe847208b1c8be3deec1824fb6526c4caa26e5
marlleyck <marlleyck@hotmail.com>
2022-11-07 13:33:56 -0300

    feat: add routes

---
+++ src/routes/Models/index.ts
@@ -0,0 +1,8 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RouteProp } from '@react-navigation/native';
+
+export type PropsNavigationStack = {
+    Home: undefined;
+}
+
+export type PropsStack = NativeStackNavigationProp<PropsNavigationStack>;
---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { PropsNavigationStack } from "./Models";
+
+const Stack = createNativeStackNavigator<PropsNavigationStack>()
